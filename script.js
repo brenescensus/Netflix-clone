a8f2f9a51ae5bedc08f7f19597a7b19113e477cf
brenescensus <76098751+brenescensus@users.noreply.github.com>
2025-06-06 02:21:31 +0300

    movie cards

---
+++ script.js
@@ -0,0 +1,16 @@
+window.assEventListener('scroll',()=>{
+    const navbar=document.querySelector('.navbar');
+    if(window.scrollY>100){
+        navbar.classList.add('scrolled');
+    }
+    else{
+        navbar.classList.remove('scrolled');
+    }
+})
+
+
+const profileIcon = document.querySelector('.profile-pic'); 
+profileIcon.addEventListener('click', () => {
+window.location.href = "profile.html";
+});
+
